8e5071804ae47fad6ef56200c6e0d778a02b8cea
Jamie Humphries <jamie.humphries@softwire.com>
2018-06-12 12:19:30 +0100

    Basic system tray application.

---
+++ RemindsSME.Desktop/ViewModels/TaskbarIconViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace RemindsSME.Desktop.ViewModels
+{
+    public class TaskbarIconViewModel
+    {
+        public void Quit()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
