4ac3e5ad15ae9e6798aafcdfd7782a5f5cbdbfff
Raam Chauhan <raam.chauhan@softwire.com>
2018-06-15 10:53:00 +0100

    Add logic for registering locking and unlocking computer.

--- RemindSME.Desktop/Bootstrapper.cs
+++ RemindSME.Desktop/Bootstrapper.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using Caliburn.Micro;
 using RemindSME.Desktop.ViewModels;
 
--- RemindSME.Desktop/ViewModels/TaskbarIconViewModel.cs
+++ RemindSME.Desktop/ViewModels/TaskbarIconViewModel.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Threading;
+using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Net;
 using Quobject.SocketIoClientDotNet.Client;
 using RemindSME.Desktop.Properties;
@@ -22,8 +23,25 @@ namespace RemindSME.Desktop.ViewModels
             timer.Start();
 
             var network = NetworkListManager.GetNetworks(NetworkConnectivityLevels.Connected).FirstOrDefault()?.Name;
-            socket = IO.Socket("http://localhost:5000");
+//            socket = IO.Socket("https://reminds-me-server.herokuapp.com/");
+            socket = IO.Socket("http://localhost:5000/");
             socket.Emit("join", network);
+
+            Microsoft.Win32.SystemEvents.SessionSwitch += new Microsoft.Win32.SessionSwitchEventHandler(SystemEventsSessionSwitch);
+
+            void SystemEventsSessionSwitch(object sender, Microsoft.Win32.SessionSwitchEventArgs e)
+            {
+                if (e.Reason == SessionSwitchReason.SessionLock)
+                {
+                    socket.Emit("sessionLock");
+                    Console.WriteLine(@"locked");
+                }
+                else if (e.Reason == SessionSwitchReason.SessionUnlock)
+                {
+                    socket.Emit("sessionUnlock");
+                    Console.WriteLine(@"unlocked");
+                }
+            }
         }
 
         public void SeeNetworkDetails()
