5d507dd9bc99b34f31e6eed97fbf73dd00cdd518
Raam Chauhan <raam.chauhan@softwire.com>
2018-06-26 17:07:26 +0100

    Handle start-up near scheduled hibernation time case.

--- RemindSME.Desktop/Bootstrapper.cs
+++ RemindSME.Desktop/Bootstrapper.cs
@@ -22,8 +22,8 @@ namespace RemindSME.Desktop
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
-            // If the next hibernation time is today, but in the past, then bump it along to the default time tomorrow
-            if (Settings.Default.NextHibernationTime < DateTime.Now)
+            // If the next hibernation time is today, but in the past, or within the next 15 minutes, then bump it along to the default time tomorrow
+            if (Settings.Default.NextHibernationTime < DateTime.Now.AddMinutes(15))
             {
                 Settings.Default.NextHibernationTime = DateTime.Today.AddDays(1).Add(Settings.Default.DefaultHibernationTime);
                 Settings.Default.Save();
