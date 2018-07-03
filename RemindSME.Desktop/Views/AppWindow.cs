508fd3d19e599201264d53c0c7f5c9ff6188e679
Raam Chauhan <raam.chauhan@softwire.com>
2018-07-03 16:53:20 +0100

    Add comment to AppWindow class.

--- RemindSME.Desktop/Views/AppWindow.cs
+++ RemindSME.Desktop/Views/AppWindow.cs
@@ -3,4 +3,6 @@
 namespace RemindSME.Desktop.Views
 {
     public class AppWindow : Window { }
+    // This class is for interactive windows, like the HubView and WelcomeView. It is used to check whether these windows are open
+    // and to differentiate between these and notification views. 
 }
